b4653178d7b01e1a4dedb9965431c821b66440b1
Khosro Hosseinabadi <khosro.h2011@gmail.com>
2024-12-08 15:45:01 +0330

    Update navbar component

--- Projects/05 auth-fastapi-react/frontend/src/components/Navbar.jsx
+++ Projects/05 auth-fastapi-react/frontend/src/components/Navbar.jsx
@@ -5,23 +5,37 @@ import { AuthContext } from "../contexts/AuthContext.jsx";
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
   return (
-    <nav className="max-w-3xl mx-auto flex items-center justify-between p-4 bg-gray-900 text-white">
+    <nav className="max-w-3xl mx-auto flex items-center p-4 bg-gray-900 text-white justify-between h-16">
       <ul className="flex space-x-4">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/register">Register</Link>
+          <Link to="/profile">Profile</Link>
         </li>
+      </ul>
+      <ul className="flex space-x-4 items-center">
+        {!user && (
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        )}
         <li>
           {user ? (
-            <button onClick={logout}>Logout</button>
+            <button
+              onClick={logout}
+              className="bg-gray-800 text-white border-0 py-2 px-4 cursor-pointer rounded-md transition duration-200 ease-in-out"
+            >
+              Logout
+            </button>
           ) : (
-            <Link to="/login">Login</Link>
+            <Link
+              to="/login"
+              className="bg-gray-800 text-white border-0 py-2 px-4 cursor-pointer rounded-md transition duration-200 ease-in-out"
+            >
+              Login
+            </Link>
           )}
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
         </li>
       </ul>
     </nav>
